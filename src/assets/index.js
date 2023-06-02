9b409cd383d4d7ef6ee54317951cab69718bd0c0
Santos Alarcón Asensio <santosalarcon86@gmail.com>
2023-06-02 21:17:04 +0200

    Update index.js

--- src/assets/index.js
+++ src/assets/index.js
@@ -26,9 +26,9 @@ import shopify from "./company/shopify.png";
 import starbucks from "./company/starbucks.png";
 import tesla from "./company/tesla.png";
 
-import carrent from "./carrent.png";
-import jobit from "./jobit.png";
-import tripguide from "./tripguide.png";
+import carrent from "./carrent.jpg";
+import jobit from "./jobit.jpg";
+import tripguide from "./tripguide.jpg";
 
 export {
   logo,

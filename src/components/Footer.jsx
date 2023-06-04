e838e40013da6aab16bc4eaa04dbb596dbbf4242
Santos Alarcón <santosalarcon86@gmail.com>
2023-06-04 11:43:56 +0200

    Fixed social icons size

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,21 +1,39 @@
 import React from 'react'
 import gmail from "../assets/gmail.svg"
 import linkedin from "../assets/linkedin.svg"
-import {github}  from "../assets/index"
+import github from "../assets/github.svg"
 import cv from "../assets/curriculum.svg"
 
 const Footer = () => {
-  return (
-    <footer className='mx-auto px-6 py-8 bg-black-100 flex flex-col justify-center max-sm:text-[14px] text-[18px] gap-3'>
+    return (
+        <footer className='mx-auto px-6 py-8 bg-black-100 flex flex-col justify-center max-sm:text-[14px] text-[18px] gap-5'>
             <div className='flex justify-center items-center gap-5'>
-               <a href="mailto:santosalarcon86@gmail.com"><img className='h-[48px] hover:scale-125 transition' src={gmail} alt="Correo electrónico" /></a>
-               <a href="https://www.linkedin.com/in/santos-alarcon-asensio" target='_blank'><img className='w-[40px] h-[40px] hover:scale-125 transition' src={linkedin} alt="LinkedIn" /></a>
-               <a href="https://github.com/SantosAlarcon" target="_blank"><img className='w-[48px] h-[48px] hover:scale-125 transition' src={github} alt="GitHub" /></a>
-               <a href="/CurriculumFullStack.pdf"><img className='w-[48px] h-[48px] hover:scale-125 transition' src={cv} alt="Curriculum Vitae" /></a>
+                <a href="mailto:santosalarcon86@gmail.com">
+                    <svg id="social" className='w-[36px] h-[36px] hover:scale-125 transition'>
+                        <use width="36px" height="36px" href={`${gmail}#social`} alt="Gmail"></use>
+                    </svg>
+                </a>
+
+                <a href="https://www.linkedin.com/in/santos-alarcon-asensio" target='_blank'>
+                    <svg id="social" className='w-[36px] h-[36px] hover:scale-125 transition'>
+                        <use href={`${linkedin}#social`} alt="LinkedIn"></use>
+                    </svg>
+
+                </a>
+                <a href="https://github.com/SantosAlarcon" target="_blank">
+                    <svg id="social" className='w-[36px] h-[36px] hover:scale-125 transition'>
+                        <use width="36px" height="36px" href={`${github}#social`} alt="GitHub" />
+                    </svg>
+                </a>
+                <a href="/CurriculumFullStack.pdf">
+                    <svg id="social" className='w-[36px] h-[36px] hover:scale-125 transition'>
+                        <use width="36px" height="36px" href={`${cv}#social`} alt="Curriculum Vitae" />
+                    </svg>
+                </a>
             </div>
             <p className='text-center'>@ {new Date().getFullYear()}  <b>Santos Alarcón Asensio</b> - Todos los derechos reservados.</p>
         </footer>
-  )
+    )
 }
 
 export default Footer

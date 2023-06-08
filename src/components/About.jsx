aa5112e7413869e5000384572f05c6b2f7be903a
Santos Alarcón <santosalarcon86@gmail.com>
2023-06-08 15:47:44 +0200

    Added overflow hidden to mobile on about page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,7 +11,7 @@ const ServiceCard = ({ title, icon, index }) => {
     return (
         <Tilt className="xs:w-[250px] w-full select-none">
             <motion.div
-                variants={slideIn("right", "spring", 0.5 * index, 0.75)}
+                variants={fadeIn("right", "spring", 0.5 * index, 0.75)}
                 className="w-full green-pink-gradient p-[1px] rounded-[20px] shadow-card"
             >
                 <div
@@ -30,7 +30,7 @@ const ServiceCard = ({ title, icon, index }) => {
 
 const About = () => {
     return (
-        <div className="overflow-hidden">
+        <div className="max-sm:overflow-hidden">
             <motion.div variants={textVariant()}>
                 <p className={styles.sectionSubText}>Introducción</p>
                 <h2 className={styles.sectionHeadText}>Sobre mí</h2>
